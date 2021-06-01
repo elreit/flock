f5a6910ccc54f5af6968e9da3bea1bd71bca8332
Luisa von Funcke <75794762+LuisaFuncke@users.noreply.github.com>
2021-06-01 21:01:04 +0100

    some changes

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,2 +1,15 @@
 class DestinationsController < ApplicationController
+
+  def new
+    @destination = Destination.new(destination_params)
+    @destination.user = current_user
+    @destination.save
+      redirect_to destinations_path(@destinations)
+  end
+
+  private
+
+  def destination_params
+    params.require(:destination).permit(:start_point, :end_point)
+  end
 end
