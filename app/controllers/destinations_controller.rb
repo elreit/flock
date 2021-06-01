02d3ca46b3a7b31fc46421f315ae60c30ac3f7c8
Mai Hoang <maihh97@gmail.com>
2021-06-01 21:39:14 +0100

    devise updating info and update destination status

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,2 +1,21 @@
 class DestinationsController < ApplicationController
+  def show
+    @destination = Destination.find(params[:id])
+  end
+  
+  def update
+    @destination = Destination.find(params[:id])
+    @destination.started = true
+    @walk = Walk.where(user_destination_id: params[:id])
+    @destination.save
+    redirect_to walk_path(@walk)
+  end
+
+  def arrived
+    @destination = @destination = Destination.find(params[:id])
+    @destination.arrived = true
+    @walk = Walk.where(user_destination_id: params[:id])
+    @destination.save
+    #redirect to review page
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
-  resources :destinations, only: [:index, :new, :create, :update]
+  resources :destinations, only: [:index, :new, :create, :update, :arrived, :show]
   resources :walks, only: [:create, :show, :update]
 end
