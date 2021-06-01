4c65cb2c617226cc5862e42040ec635fd9cf079c
Mai Hoang <maihh97@gmail.com>
2021-06-01 20:43:38 +0100

    adding fields to devise sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo, :name, :description, :emergency_contact_name, :emergency_contact_number])
   end
 end
