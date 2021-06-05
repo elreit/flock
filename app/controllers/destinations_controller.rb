daa78443dfddd5bdf74337ffcc66745a1d30d1b7
rosita alvarado <arositado@gmail.com>
2021-06-05 16:35:08 +0100

    destinations controller updates to new, create, index

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -3,6 +3,7 @@ class DestinationsController < ApplicationController
     @destination = Destination.new
     @location = Location.new
   end
+# testing
 
   def create
     @start_location = Location.create(address: params[:destination][:start_location])
