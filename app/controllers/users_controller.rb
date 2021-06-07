a051cf1ba1f618e861eda8199a9840372e35d843
Mai Hoang <maihh97@gmail.com>
2021-06-07 23:31:04 +0100

    updating the dashboard method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,16 @@ class UsersController < ApplicationController
   def dashboard
     # Walk request I sent, I hold user_destination_id
     my_destination = Destination.where(user_id: current_user.id).last
-    @my_walk_request = Walk.where(user_destination_id: my_destination.id).last
-    my_buddy_destination = Destination.where(id: @my_walk_request.buddy_destination_id).last
-    @my_buddy = my_buddy_destination.user
+    if my_destination
+      @my_walk_request = Walk.where(user_destination_id: my_destination.id).last
+      my_buddy_destination = Destination.where(id: @my_walk_request.buddy_destination_id).last
+      @my_buddy = my_buddy_destination.user
+    end
     # Walk request I received, I hold buddy_destination_id
     @walk_request_received = Walk.where(buddy_destination_id: my_destination.id).last
-    requester_destination = Destination.where(id: @walk_request_received.user_destination_id).last
-    @requester = requester_destination.user
+    if @walk_request_received
+      requester_destination = Destination.where(id: @walk_request_received.user_destination_id).last
+      @requester = requester_destination.user
+    end
   end
 end
