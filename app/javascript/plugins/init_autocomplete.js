45fe22989672615a73a533a8ba3829a8b220b407
Mai Hoang <maihh97@gmail.com>
2021-06-09 00:15:53 +0100

    update mapbox js, user dashboard and walk model

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,9 +1,19 @@
 import places from 'places.js';
 
 const initAutocomplete = () => {
-  const addressInput = document.getElementById('flat_address');
-  if (addressInput) {
-    places({ container: addressInput });
+  // When creating destinations
+  const startLocation = document.getElementById('destination_start_location');
+  if (startLocation) {
+    places({ container: startLocation });
+  }
+  const endLocation = document.getElementById('destination_end_location');
+  if (endLocation) {
+    places({ container: endLocation });
+  }
+ // When entering the meeting point
+  const meetingPoint = document.getElementById('walk_meeting_point');
+  if (meetingPoint) {
+    places({ container: meetingPoint });
   }
 };
 
