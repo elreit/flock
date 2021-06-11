d3e508cf367133c8467a4a646dd4a87ca798728e
Mai Hoang <maihh97@gmail.com>
2021-06-11 23:31:11 +0100

    new home page, front end for destination new, login and create a new user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @destinations = Destination.all
+    if current_user
+      redirect_to new_destination_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'destinations#new'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
   resources :destinations, only: [:index, :new, :create, :update] do
