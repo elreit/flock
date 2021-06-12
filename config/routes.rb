1ce0c4c2c96a2bb2fdc233d362dc2880ed975282
rosita alvarado <arositado@gmail.com>
2021-06-12 10:51:28 +0100

    updated routes to nest favorite gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'destinations#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
   resources :destinations, only: [:index, :new, :create, :update] do
@@ -8,13 +8,10 @@ Rails.application.routes.draw do
       patch "arrived"
     end
   end
-  resources :walks, only: [:create, :show, :update]
+  resources :walks, only: [:create, :show, :update] do
+    member do
+      post 'toggle_favorite', to: "walks#toggle_favorite"
+    end
+  end
   get 'dashboard', to: 'users#dashboard'
 end
-
-
-#  resources :walks, only: [:create, :show, :update]
-#   member do
-#     post 'toggle_favorite', to: "walks#toggle_favorite"
-#   end
-# end
