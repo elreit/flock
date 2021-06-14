ac97ecec0072a8d941ddd1b56c471abaf2606463
Emma Reitman <em_06@hotmail.co.uk>
2021-06-14 17:01:47 +0100

    autocomplete now with postcode and uk only

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,19 +1,30 @@
 import places from 'places.js';
 
 const initAutocomplete = () => {
+  //Return location in GB only
+
   // When creating destinations
   const startLocation = document.getElementById('destination_start_location');
   if (startLocation) {
-    places({ container: startLocation });
+    places({ container: startLocation }).configure({
+    postcodeSearch: true,
+    countries: ['gb'],
+    language: 'en' });
   }
   const endLocation = document.getElementById('destination_end_location');
   if (endLocation) {
-    places({ container: endLocation });
+    places({ container: endLocation }).configure({
+    postcodeSearch: true,
+    countries: ['gb'],
+    language: 'en' });
   }
  // When entering the meeting point
   const meetingPoint = document.getElementById('walk_meeting_point');
   if (meetingPoint) {
-    places({ container: meetingPoint });
+    places({ container: meetingPoint }).configure({
+    postcodeSearch: true,
+    countries: ['gb'],
+    language: 'en' });
   }
 };
 
