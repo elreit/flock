164be0c6e1bc77d65976f521d096f78ff4ffdb01
rosita alvarado <arositado@gmail.com>
2021-06-15 19:55:59 +0100

    updated autocomplete to stop error

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -6,25 +6,16 @@ const initAutocomplete = () => {
   // When creating destinations
   const startLocation = document.getElementById('destination_start_location');
   if (startLocation) {
-    places({ container: startLocation }).configure({
-    postcodeSearch: true,
-    countries: ['gb'],
-    language: 'en' });
+    places({ container: startLocation, countries: ['gb']});
   }
   const endLocation = document.getElementById('destination_end_location');
   if (endLocation) {
-    places({ container: endLocation }).configure({
-    postcodeSearch: true,
-    countries: ['gb'],
-    language: 'en' });
+    places({ container: endLocation, countries: ['gb']});
   }
  // When entering the meeting point
   const meetingPoint = document.getElementById('walk_meeting_point');
   if (meetingPoint) {
-    places({ container: meetingPoint }).configure({
-    postcodeSearch: true,
-    countries: ['gb'],
-    language: 'en' });
+    places({ container: meetingPoint, countries: ['gb'] });
   }
 };
 
