7fb072d7bdd758a21cb2db8362fac1f8ae847acf
rosita alvarado <arositado@gmail.com>
2021-06-22 19:31:02 +0100

    added method to review

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   validates :name, :description, :emergency_contact_name, :emergency_contact_number, presence: true
   validates :emergency_contact_number, format: { with: /\A[1-9]{2}\d{10}\z/, message: "Please enter a valid phone number" }
 
-  def reviews
+  def average_rating
     reviews = Review.where(reviewee_id: id)
     avg_rating = 'No reviews'
     sum_rating = 0
@@ -27,18 +27,18 @@ class User < ApplicationRecord
     avg_rating
   end
 
-  def walks
-    total_walks = 0
-    # get all past destinations
-    past_destinations = Destination.where(user_id: id)
-    # find walks that have user_destination && walk_request_status accepted
-    past_destinations.each do |dest|
-      walks_as_requestor = Walk.where(user_destination_id: dest.id, walk_request_status: "Accepted").count
-      total_walks += walks_as_requestor
-    # find walks that have buddy_destination && walk_request_status accepted
-      walks_as_buddy = Walk.where(buddy_destination_id: dest.id, walk_request_status: "Accepted").count
-      total_walks += walks_as_buddy
-    end
-    total_walks
-  end
+  # def walks
+  #   total_walks = 0
+  #   # get all past destinations
+  #   past_destinations = Destination.where(user_id: id)
+  #   # find walks that have user_destination && walk_request_status accepted
+  #   past_destinations.each do |dest|
+  #     walks_as_requestor = Walk.where(user_destination_id: dest.id, walk_request_status: "Accepted").count
+  #     total_walks += walks_as_requestor
+  #   # find walks that have buddy_destination && walk_request_status accepted
+  #     walks_as_buddy = Walk.where(buddy_destination_id: dest.id, walk_request_status: "Accepted").count
+  #     total_walks += walks_as_buddy
+  #   end
+  #   total_walks
+  # end
 end
